2f00c9635851a64003271744cfdedde4cee64dfb
葛的mac <gml@gedeMacBook-Air.local>
2022-08-08 10:48:39 +0800

    配置设置文件及数据库

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -1,3 +1,6 @@
+# 开发阶段配置
+
+
 """
 Django settings for meiduo_mall project.
 
@@ -11,10 +14,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'  # DRF框架
 ]
 
 MIDDLEWARE = [
@@ -69,17 +72,71 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '127.0.0.1',  # 主机名
+        'PORT': 3306,  # 端口号
+        'USER': 'root',  # 用户权限
+        'PASSWORD': '176294955',  # 数据库密码
+        'NAME': 'meiduo',  # 创建的数据库名
     }
 }
 
+# 配置redis数据库作为缓存后端
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }, "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
+# ⽇日志
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁⽤用已经存在的⽇日志器器
+    'formatters': {  # ⽇日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'},
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'},
+    },
+    'filters': {  # 对⽇日志进⾏行行过滤
+        'require_debug_true': {  # django在debug模式下才输出⽇日志
+            '()': 'django.utils.log.RequireDebugTrue', },
+    },
+    'handlers': {  # ⽇日志处理理⽅方法
+        'console': {  # 向终端中输出⽇日志 'level': 'INFO',
+            'filters': ['require_debug_true'], 'class': 'logging.StreamHandler', 'formatter': 'simple'
+        },
+        'file': {  # 向⽂文件中输出⽇日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), "logs/meiduo.log"),
+            # ⽇日志⽂文件的位置 'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        }, },
+    'loggers': {  # ⽇日志器器
+        'django': {  # 定义了了⼀一个名为django的⽇日志器器
+            'handlers': ['console', 'file'],  # 可以同时向终端与⽂文件中输出⽇日志 'propagate': True, # 是否继续传递⽇日志信息
+            'level': 'INFO',  # ⽇日志器器接收的最低⽇日志级别
+        }, }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,20 +156,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
